b7716e33c9fca74f935200afb222abfa672cfd23
Pranjal Mathur <pjmathur157@gmail.com>
2022-07-03 11:03:10 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,273 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+
+  <title>Hello, world!</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <?php
+  require_once('nav.php');
+  ?>
+  <section class="main1 container-fluid px-0 mx-0">
+    <div class="overlay text-center text-white pt-5">
+      <h2 class=" ">
+        YOU CAN TRUST YOUR CAR TO US</h2>
+      <h1>Advanced Vehicle Repair Services</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      <button class="btn">DISCOVER MORE </button>
+    </div>
+  </section>
+  <section class="main2 py-5">
+    <div class="container py-4 ">
+      <div class="row mx-5">
+        <div class="col-lg-6">
+          <h2>About Our Garage</h2>
+          <div class="underborder"></div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut urna tincidunt dui lobortis bibendum. Sed dui tortor, dapibus ut auctor sit amet, cursus id odio. Duis ornare mattis lectus eu aliquam.</p>
+          <p class="mb-4">Morbi facilisis tellus nec vestibulum pretium. Cras tempor arcu non nibh blandit facilisis. Donec elementum quam euismod dolor varius molestie. Nulla facilisi mattis pretium erat.</p>
+          <button class="btn">READ MORE </button>
+
+        </div>
+        <div class="col-lg-6 ps-5">
+          <img src="img/aboutus.png" class="img-fluid" alt="">
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <section class="main3 ">
+    <div class="overlay2 text-white py-5">
+      <div class="container py-5">
+        <div class="row mx-5">
+          <div class="col-lg-6 d-flex flex-wrap">
+            <div class="counter-container">
+
+              <div class="counter" data-target="12000"></div>
+              <h4>Happy Client</h4>
+            </div>
+
+            <div class="counter-container" style="background-color:rgba(255,255,255,0.2) ;">
+
+              <div class="counter" data-target="5000"></div>
+              <h4>Branch Office</h4>
+
+              <!-- <span>YouTube Subscribers</span> -->
+            </div>
+
+            <div class="counter-container " style="background-color:rgba(255,255,255,0.2) ;">
+
+              <div class="counter" data-target="7500"></div>
+              <h4>Technicians</h4>
+
+              <!-- <span>Facebook Fans</span>
+</div> -->
+
+            </div>
+            <div class="counter-container">
+
+              <div class="counter" data-target="400"></div>
+              <h4>Get Awards</h4>
+
+              <!-- <span>Facebook Fans</span> -->
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <h2>About Our Garage</h2>
+            <div class="underborder"></div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut urna tincidunt dui lobortis bibendum. Sed dui tortor, dapibus ut auctor sit amet, cursus id odio. Duis ornare mattis lectus eu aliquam. Morbi facilisis tellus nec vestibulum pretium.</p>
+            <p class="mb-4">Cras tempor arcu non nibh blandit facilisis. Donec elementum quam euismod dolor varius molestie. Nulla facilisi.</p>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </section>
+  <section class="main4 py-5">
+    <h2 class="pt-3 text-center">Treatment Car Services</h2>
+    <div class="underborder2"></div>
+    <div class="container">
+      <div class="row mx-5">
+        <div class="col-lg-3">
+          <div class="card">
+            <img src="img/Icon-car-4.png" class="card-img-top img-fluid my-3" alt="...">
+            <div class="card-body text-center">
+              <h5 class="card-title">System Service</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button class="btn">READ MORE </button>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card">
+            <img src="img/Icon-car-3.png" class="card-img-top img-fluid my-3" alt="...">
+            <div class="card-body text-center">
+              <h5 class="card-title">Engine Setting</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button class="btn">READ MORE </button>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card">
+            <img src="img/Icon-car-2.png" class="card-img-top img-fluid my-3" alt="...">
+            <div class="card-body text-center">
+              <h5 class="card-title">Tyre Checking</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button class="btn">READ MORE </button>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card">
+            <img src="img/Icon-car-1.png" class="card-img-top img-fluid my-3" alt="...">
+            <div class="card-body text-center">
+              <h5 class="card-title">Restoration</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button class="btn">READ MORE </button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="main5 ">
+    <div class="overlay3 py-5 text-center text-white">
+      <div class="owl-carousel owl-theme">
+        <div class="item">
+          <h1>
+            What Our Clients Have To Say</h1>
+          <i class="fa-solid fa-quote-left"></i>
+          <p>Suspendisse in scelerisque leo, quis finibus tellus. Suspendisse potenti. Aliquam gravida orci lectus, ut rhoncus dui consectetur quis. Phasellus at turpis a ante ullamcorper elementum. Donec tristique posuere ligula. Pellentesque pretium pellentesque quam.</p>
+          <div class="smallcontainer d-flex justify-content-center">
+            <div class="pimg">
+
+            </div>
+            <span class="pt-4 ps-2">Paul Howard<BR />
+              Happy Client</span>
+
+          </div>
+        </div>
+        <div class="item">
+          <h1>
+            What Our Clients Have To Say</h1>
+          <i class="fa-solid fa-quote-left"></i>
+          <p>Suspendisse in scelerisque leo, quis finibus tellus. Suspendisse potenti. Aliquam gravida orci lectus, ut rhoncus dui consectetur quis. Phasellus at turpis a ante ullamcorper elementum. Donec tristique posuere ligula. Pellentesque pretium pellentesque quam.</p>
+          <div class="smallcontainer d-flex justify-content-center">
+            <div class="pimg">
+
+            </div>
+            <span class="pt-4 ps-2">Paul Howard<BR />
+              Happy Client</span>
+
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+  </section>
+  <footer class="py-5">
+    <div class="container">
+      <div class="row mx-5">
+
+        <div class="col-lg-4 me-3">
+          <h2>About Us</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut urna tincidunt dui lobortis bibendum. Duis ornare mattis lectus eu aliquam. Morbi facilisis tellus nec vestibulum pretium.</p>
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-linkedin-in"></i>
+          <i class="fa-brands fa-instagram"></i>
+        </div>
+        <div class="col-lg-3 me-4">
+          <h2>Contact Us</h2>
+          <p>Address: 4912 Hickory Street, Utah, 84003, American Fork.</p>
+          <p>Phone: +1 2345 67 890<br>
+            E-mail: support@reparo.com</p>
+
+        </div>
+        <div class="col-lg-4 me-3">
+          <h2>Newsletter</h2>
+          <p>Sign up to our newsletter to receive the latest news and special discounts.</p>
+          <button class="btn btn1 ">READ MORE </button>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="js/owl.carousel.js"></script>
+  <script>
+    $(document).ready(function() {
+      var owl = $('.owl-carousel');
+      owl.owlCarousel({
+
+        nav: false,
+        loop: true,
+        dots: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 3
+          },
+          1000: {
+            items: 1
+          }
+        }
+      })
+    })
+    const counters = document.querySelectorAll(".counter");
+
+    counters.forEach((counter) => {
+      counter.innerText = "0";
+
+      const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+        const c = +counter.innerText;
+
+        const increment = target / 200;
+
+        if (c < target) {
+          counter.innerText = `${Math.ceil(c + increment)}`;
+          setTimeout(updateCounter, 1);
+        } else {
+          counter.innerText = target;
+        }
+      };
+
+      updateCounter();
+    });
+  </script>
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> -->
+   
+</body>
+
+</html>
